f79209f286dadb31228e8e86ce33f27eb9e7098a
LZH <liu35683568@gmail.com>
2024-08-10 13:15:37 +0800

    计算书费

--- J2/J2-10/A.cpp
+++ J2/J2-10/A.cpp
@@ -1,3 +1,4 @@
+//计算书费
 #include<iostream>  //导入头文件
 #include<cmath>
 #include<iomanip>
@@ -5,15 +6,13 @@
 #include<ctime>
 using namespace std;  //使用命名空间
 int main() {
-    double A[10]={28.9, 32.7, 45.6, 78, 35, 86.2, 27.8, 43, 56, 65}, B[10], x=0;
+    double A[10]={28.9, 32.7, 45.6, 78, 35, 86.2, 27.8, 43, 56, 65}, x=0;
     int temp;
-for (int i = 0; i < 10; i++)
-    cin>>B[i];
-for (int y = 0; y < 10; y++)
-{
-    temp=B[y];
-    x+=A[temp];
+for (int i = 0; i < 10; i++){
+    cin>>temp;
+    x+=A[i]*temp;
 }
+
 cout<<x;
 
  	return 0 ;  //程序结束
